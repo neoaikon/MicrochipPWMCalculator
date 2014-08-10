d797fd8c4a4b93c955590ab8fc8fa5aa0fdf66b7
Michael D. Stone <mstone@moarobotics.com>
2014-08-10 13:51:42 -0400

    Initial commit

---
+++ MicrochipPWMCalculator/frmMain.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace MicrochipPWMCalculator
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFreq = new System.Windows.Forms.TextBox();
+            this.txtPWMFreq = new System.Windows.Forms.TextBox();
+            this.txtDivs = new System.Windows.Forms.TextBox();
+            this.lblFreq = new System.Windows.Forms.Label();
+            this.lblPWMFreq = new System.Windows.Forms.Label();
+            this.lblDivs = new System.Windows.Forms.Label();
+            this.txtCountsP = new System.Windows.Forms.TextBox();
+            this.lblCountsP = new System.Windows.Forms.Label();
+            this.lblCountsD = new System.Windows.Forms.Label();
+            this.txtCountsD = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.txtError = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtFreq
+            // 
+            this.txtFreq.Location = new System.Drawing.Point(146, 12);
+            this.txtFreq.Name = "txtFreq";
+            this.txtFreq.Size = new System.Drawing.Size(100, 20);
+            this.txtFreq.TabIndex = 0;
+            this.txtFreq.Text = "1000000";
+            this.txtFreq.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtPWMFreq
+            // 
+            this.txtPWMFreq.Location = new System.Drawing.Point(146, 38);
+            this.txtPWMFreq.Name = "txtPWMFreq";
+            this.txtPWMFreq.Size = new System.Drawing.Size(100, 20);
+            this.txtPWMFreq.TabIndex = 1;
+            this.txtPWMFreq.Text = "10000";
+            this.txtPWMFreq.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtDivs
+            // 
+            this.txtDivs.Location = new System.Drawing.Point(146, 64);
+            this.txtDivs.Name = "txtDivs";
+            this.txtDivs.Size = new System.Drawing.Size(100, 20);
+            this.txtDivs.TabIndex = 2;
+            this.txtDivs.Text = "100";
+            this.txtDivs.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lblFreq
+            // 
+            this.lblFreq.AutoSize = true;
+            this.lblFreq.Location = new System.Drawing.Point(33, 15);
+            this.lblFreq.Name = "lblFreq";
+            this.lblFreq.Size = new System.Drawing.Size(107, 13);
+            this.lblFreq.TabIndex = 3;
+            this.lblFreq.Text = "Processor Frequency";
+            // 
+            // lblPWMFreq
+            // 
+            this.lblPWMFreq.AutoSize = true;
+            this.lblPWMFreq.Location = new System.Drawing.Point(53, 41);
+            this.lblPWMFreq.Name = "lblPWMFreq";
+            this.lblPWMFreq.Size = new System.Drawing.Size(87, 13);
+            this.lblPWMFreq.TabIndex = 4;
+            this.lblPWMFreq.Text = "PWM Frequency";
+            // 
+            // lblDivs
+            // 
+            this.lblDivs.AutoSize = true;
+            this.lblDivs.Location = new System.Drawing.Point(83, 67);
+            this.lblDivs.Name = "lblDivs";
+            this.lblDivs.Size = new System.Drawing.Size(49, 13);
+            this.lblDivs.TabIndex = 5;
+            this.lblDivs.Text = "Divisions";
+            // 
+            // txtCountsP
+            // 
+            this.txtCountsP.BackColor = System.Drawing.SystemColors.Window;
+            this.txtCountsP.Location = new System.Drawing.Point(146, 90);
+            this.txtCountsP.Name = "txtCountsP";
+            this.txtCountsP.ReadOnly = true;
+            this.txtCountsP.Size = new System.Drawing.Size(100, 20);
+            this.txtCountsP.TabIndex = 6;
+            this.txtCountsP.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lblCountsP
+            // 
+            this.lblCountsP.AutoSize = true;
+            this.lblCountsP.Location = new System.Drawing.Point(19, 93);
+            this.lblCountsP.Name = "lblCountsP";
+            this.lblCountsP.Size = new System.Drawing.Size(121, 13);
+            this.lblCountsP.TabIndex = 7;
+            this.lblCountsP.Text = "Timer Counts Per Period";
+            // 
+            // lblCountsD
+            // 
+            this.lblCountsD.AutoSize = true;
+            this.lblCountsD.Location = new System.Drawing.Point(12, 119);
+            this.lblCountsD.Name = "lblCountsD";
+            this.lblCountsD.Size = new System.Drawing.Size(128, 13);
+            this.lblCountsD.TabIndex = 8;
+            this.lblCountsD.Text = "Timer Counts Per Division";
+            // 
+            // txtCountsD
+            // 
+            this.txtCountsD.BackColor = System.Drawing.SystemColors.Window;
+            this.txtCountsD.Location = new System.Drawing.Point(146, 116);
+            this.txtCountsD.Name = "txtCountsD";
+            this.txtCountsD.ReadOnly = true;
+            this.txtCountsD.Size = new System.Drawing.Size(100, 20);
+            this.txtCountsD.TabIndex = 9;
+            this.txtCountsD.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(146, 168);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 10;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // txtError
+            // 
+            this.txtError.BackColor = System.Drawing.SystemColors.Window;
+            this.txtError.Location = new System.Drawing.Point(146, 142);
+            this.txtError.Name = "txtError";
+            this.txtError.ReadOnly = true;
+            this.txtError.Size = new System.Drawing.Size(100, 20);
+            this.txtError.TabIndex = 12;
+            this.txtError.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Error %";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.BackgroundImage = global::MicrochipPWMCalculator.Properties.Resources.contentbg;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(256, 198);
+            this.Controls.Add(this.txtError);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.txtCountsD);
+            this.Controls.Add(this.lblCountsD);
+            this.Controls.Add(this.lblCountsP);
+            this.Controls.Add(this.txtCountsP);
+            this.Controls.Add(this.lblDivs);
+            this.Controls.Add(this.lblPWMFreq);
+            this.Controls.Add(this.lblFreq);
+            this.Controls.Add(this.txtDivs);
+            this.Controls.Add(this.txtPWMFreq);
+            this.Controls.Add(this.txtFreq);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmMain";
+            this.Text = "Microchip PWM Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFreq;
+        private System.Windows.Forms.TextBox txtPWMFreq;
+        private System.Windows.Forms.TextBox txtDivs;
+        private System.Windows.Forms.Label lblFreq;
+        private System.Windows.Forms.Label lblPWMFreq;
+        private System.Windows.Forms.Label lblDivs;
+        private System.Windows.Forms.TextBox txtCountsP;
+        private System.Windows.Forms.Label lblCountsP;
+        private System.Windows.Forms.Label lblCountsD;
+        private System.Windows.Forms.TextBox txtCountsD;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.TextBox txtError;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MicrochipPWMCalculator/frmMain.cs
@@ -0,0 +1,88 @@
+﻿/*
+ * Program: Microchip PWM Calculator
+ * Author: Michael D. Stone (AKA Neoaikon)
+ * Date: 8/10/2014
+ * Copyright (C) 2014 Mid-Ohio Area Robotics
+ * 
+ * Description: Calculates the number of timer counts
+ * required to meet a specific PWM frequency. Counts
+ * per Period is how many timer counts occur for the
+ * entire duration of the PWM signal. Counts per
+ * Division is how many timer counts occur for each
+ * step in the PWM signal (0 - Divisons). Since you
+ * cannot have fractional counts, this number is rounded
+ * up. Error % is how far off the actual PWM frequency will
+ * be from the desired PWM frequency.
+ * 
+ * GPL V3 License
+ * --------------
+ * This file is part of Microchip PWM Calculator.
+ * Foobar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Microchip PWM Calculator is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Microchip PWM Calculator.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MicrochipPWMCalculator
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Parse the (assumed) floating point values
+                float Freq = float.Parse(txtFreq.Text);
+                float PWMFreq = float.Parse(txtPWMFreq.Text);
+                float Divisions = float.Parse(txtDivs.Text);
+
+                // Counts Per Period = (1/PWMFreq)/(1/(FOSC/4)), where PWMFreq is your PWM Frequency
+                // and FOSC is your MCU Clock frequency
+                float CountsP = (1.0f / PWMFreq) / (1 / (Freq / 4));
+
+                // Counts Per Divison = (CountsPerPeriod/Divisions).
+                float CountsD = CountsP / Divisions;
+                
+                // If we get any invalid values, change the result text boxes red
+                if(CountsD < 1.0f) txtCountsD.BackColor = Color.LightPink;
+                else txtCountsD.BackColor = Color.White;                
+                if (CountsP < Divisions) txtCountsP.BackColor = Color.LightPink;
+                else txtCountsP.BackColor = Color.White;
+
+                // Parse the values into strings and put them into the text boxes.
+                txtCountsD.Text = CountsD.ToString();
+                txtCountsP.Text = CountsP.ToString();
+                // Calculate the error
+                // Error Percent = (1 - (CountsD / RoundUp(CountsD))) * 100
+                txtError.Text = Math.Round(((1-(CountsD / Math.Round(CountsD, MidpointRounding.AwayFromZero))) * 100), 2).ToString() + "%";
+            }
+            catch(Exception d)
+            {
+                MessageBox.Show("An error occured, please check your Frequency/PWM Freqency/Divisons and try again");
+            }
+        }
+    }
+}
